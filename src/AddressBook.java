558895b826d5f5c83d6b0c998166f9c674331b1e
Christina <christinaxdang@gmail.com>
2021-09-24 12:48:20 -0400

    added method

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,12 +4,24 @@ public class AddressBook {
 
     private ArrayList<BuddyInfo> addressB;
 
-    public void addBuddy(BuddyInfo buddy){
-        addressB.add(buddy);
+    public AddressBook(){
+        addressB = new ArrayList<>();
     }
 
-    public void removeBuddy(BuddyInfo buddy){
-        addressB.remove(buddy);
+    public void addBuddy(BuddyInfo buddy){
+        if(buddy != null){
+            addressB.add(buddy);
+        }
+    }
+
+    public void removeBuddy(int index){
+        if(index >= 0 && index < addressB.size()){
+            addressB.remove(index);
+        }
+    }
+
+    public void newMethod(){
+
     }
 
     public static void main(String args[]){
@@ -17,7 +29,7 @@ public class AddressBook {
         BuddyInfo budInfo = new BuddyInfo("Homer", "123 Sesame Street", "0987654321");
         AddressBook addressBk = new AddressBook();
         addressBk.addBuddy(budInfo);
-        addressBk.removeBuddy(budInfo);
+        addressBk.removeBuddy(0);
 
     }
 }
